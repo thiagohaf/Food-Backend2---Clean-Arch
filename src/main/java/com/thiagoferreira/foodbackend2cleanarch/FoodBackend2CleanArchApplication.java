4eaf29896345ef6d3bcc49a8747ba08802b2ef16
Thiago Ferreira <thiagohaf@yahoo.com.br>
2026-03-08 01:59:34 -0300

    feat: Configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/thiagoferreira/foodbackend2cleanarch/FoodBackend2CleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoferreira.foodbackend2cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackend2CleanArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBackend2CleanArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thiagoferreira/foodbackend2cleanarch/FoodBackend2CleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoferreira.foodbackend2cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBackend2CleanArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
