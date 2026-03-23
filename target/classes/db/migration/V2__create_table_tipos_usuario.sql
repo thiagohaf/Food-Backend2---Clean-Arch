12ca1d0e28dc15fb421bb0e8d662b8a23186f28c
Thiago Ferreira <thiagohaf@yahoo.com.br>
2026-03-23 20:23:36 -0300

    fix(projeto): limpando arquivos desnecessarios e atualizando .gitignore

--- target/classes/db/migration/V2__create_table_tipos_usuario.sql
+++
@@ -1,5 +0,0 @@
--- Flyway migration V2: create table tipos_usuario (espelho da TipoUsuarioEntity)
-CREATE TABLE tipos_usuario (
-    id CHAR(36) NOT NULL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL UNIQUE
-);
--- target/classes/db/migration/V5__add_disponibilidade_local_to_itens_cardapio.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE itens_cardapio
-ADD COLUMN disponibilidade_local BOOLEAN NOT NULL DEFAULT TRUE;
